1ca1c24834f7e37c85a8be6668c038ef0c91475d
J <wiemanboy@gmail.com>
2022-02-15 16:06:14 +0100

    met 2b bezig

---
+++ src/practicum/practicum2B/Voetbalclub.java
@@ -0,0 +1,28 @@
+package practicum.practicum2B;
+
+public class Voetbalclub {
+    int aantalGewonnen = 0;
+    int aantalGelijk = 0;
+    int aantalVerloren = 0;
+
+    Voetbalclub(String naam){}
+
+    public void verwerkResultaat(char ch) {
+        if (ch == 'w')
+            aantalGewonnen = aantalGewonnen + 1;
+        if (ch == 'g')
+            aantalGelijk = aantalGelijk + 1;
+        if (ch == 'v')
+            aantalVerloren = aantalVerloren + 1;
+    }
+
+    public int aantalPunten(){
+        return aantalGewonnen * 3 + aantalGelijk;
+
+    }
+
+    public int aantalGespeeld(){
+        return aantalGewonnen + aantalGelijk + aantalVerloren;
+    }
+
+}
--- src/practicum/practicum2B/practicum2b.java
+++ src/practicum/practicum2B/practicum2b.java
@@ -1,4 +1,21 @@
 package practicum.practicum2B;
 
 public class practicum2b {
+    public static void main(String[] args) {
+        Voetbalclub ajx = new Voetbalclub("Ajax      ");
+        Voetbalclub feij = new Voetbalclub("Feijenoord");
+
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('w');
+        feij.verwerkResultaat('g');
+
+        System.out.println("Feijenoord punten: " + feij.aantalPunten());
+        System.out.println("Ajax gespeeld: " + ajx.aantalGespeeld());
+        System.out.println();
+
+        System.out.println(ajx);
+        System.out.println(feij);
+    }
 }
+
