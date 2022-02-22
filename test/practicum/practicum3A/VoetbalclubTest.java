af7224cbb6375263e5ba1a1689b66f2f1f1aaab7
Bart <bart.vanderham@student.hu.nl>
2022-02-22 14:34:32 +0100

    Testuitbreiding gecommit

--- test/practicum/practicum3A/VoetbalclubTest.java
+++ test/practicum/practicum3A/VoetbalclubTest.java
@@ -51,5 +51,43 @@ class VoetbalclubTest {
         assertEquals(4,feij.aantalPunten());
     }
 
+    @Test
+    void checkIfRepeatedResultAltersPoints()
+    {
+        String testrow = createTestString();
+        Integer expectedPoints = determineExpectedPoints(testrow);
+        for(char i: testrow.toCharArray())
+        {
+            v.verwerkResultaat(i);
+        }
+        assertEquals(expectedPoints, v.aantalPunten());
+    }
+
+    private String createTestString()
+    {
+        String res = "";
+        Random rand = new Random();
+        int r = 0;
+        for(int i = 0; i < 10; i++)
+        {
+            r = rand.nextInt(3) + 1;
+            if(r == 1){res += "v";}
+            else if(r == 2){res += "g";}
+            else{res += "w";}
+        }
+        return res;
+    }
+
+    private Integer determineExpectedPoints(String results)
+    {
+        int res = 0;
+        for(char target: results.toCharArray())
+        {
+            if(target == 'g') {res += 1;}
+            else if (target == 'w'){res += 3;}
+        }
+        return res;
+    }
+
 
 }
