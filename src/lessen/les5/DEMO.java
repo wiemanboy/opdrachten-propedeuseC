8a712255289f078a5bd414a2045eaf422545fa3e
J <wiemanboy@gmail.com>
2022-02-22 15:38:02 +0100

    les 5

---
+++ src/lessen/les5/DEMO.java
@@ -0,0 +1,13 @@
+package lessen.les5;
+import java.util.ArrayList;
+
+public class DEMO {
+    public static void main(String[] args) {
+        ArrayList lst = new ArrayList();
+        lst.add(5);
+        lst.add(10);
+        System.out.println(lst);
+
+
+    }
+}
