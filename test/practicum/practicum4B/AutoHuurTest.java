cb51ce97c5ea704281d76e650c92331c35344590
J <wiemanboy@gmail.com>
2022-02-24 13:06:53 +0100

    bezig met 4B test

---
+++ test/practicum/practicum4B/AutoHuurTest.java
@@ -0,0 +1,15 @@
+package practicum.practicum4B;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AutoHuurTest {
+    AutoHuur a1;
+
+    @BeforeEach
+    public void init(){
+        a1 = new AutoHuur();
+    }
+
+}
