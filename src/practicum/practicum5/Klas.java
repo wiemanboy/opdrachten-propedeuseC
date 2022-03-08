a5391affc2838011763d2703c50fe97807a2ecf7
J <wiemanboy@gmail.com>
2022-03-08 15:37:00 +0100

    5 af

---
+++ src/practicum/practicum5/Klas.java
@@ -0,0 +1,41 @@
+package practicum.practicum5;
+
+import java.util.ArrayList;
+
+public class Klas {
+    private String klasCode;
+    private ArrayList<Leerling> deLeerlingen = new ArrayList<Leerling>();
+
+    public Klas(String kC){
+        klasCode = kC;
+    }
+
+    public void voegLeerlingToe(Leerling l){
+        deLeerlingen.add(l);
+    }
+
+    public void wijzigCijfer(String nm, double nweCijfer){
+        for (Leerling leerling : deLeerlingen) {
+            if (leerling.getNaam().equals(nm)){
+                leerling.setCijfer(nweCijfer);
+            }
+        }
+    }
+
+    public ArrayList<Leerling> getLeerlingen(){
+        return deLeerlingen;
+    }
+
+    public int aantalLeerlingen(){
+        return deLeerlingen.size();
+    }
+
+    public String toString(){
+        String s = "In klas " + klasCode + " zitten de volgende leerlingen: \n";
+        for (Leerling leerling : deLeerlingen) {
+            s += leerling + "\n";
+        }
+        return s;
+    }
+
+}
