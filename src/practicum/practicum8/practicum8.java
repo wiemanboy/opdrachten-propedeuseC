4940ca94f776d6226737ac2114114f8ec8cec98d
J <wiemanboy@gmail.com>
2022-03-17 15:05:03 +0100

    8 af

--- src/practicum/practicum8/practicum8.java
+++ src/practicum/practicum8/practicum8.java
@@ -14,7 +14,7 @@ public class practicum8 {
         Goed f1 = new Fiets("mountainbike",1000,2019,123456);
         Goed f2 = new Fiets("mountainbike",1000,2019,123456);
         Goed f3 = new Fiets("gazelle",600,2017,123457);
-        Goed f4 = new Fiets("fiets can 9 karaat goud",7000000,2024,9016);
+        Goed f4 = new Fiets("fiets can 12 karaat goud",47000000,2024,9016);
 
         bI1.schafAan(a1);
         bI1.schafAan(a2);
