8b89ef3fc0f4012543eddfc6921a6f3ed8ed5813
J <wiemanboy@gmail.com>
2022-03-20 12:05:41 +0100

    begin 9B

--- src/practicum/practicum9B/userinterface/BoekingenController.java
+++ src/practicum/practicum9B/userinterface/BoekingenController.java
@@ -1,4 +1,12 @@
 package practicum.practicum9B.userinterface;
 
+import javafx.event.ActionEvent;
+
 public class BoekingenController {
+
+
+
+    public void boek(ActionEvent actionEvent){}
+
+    public void reset(ActionEvent actionEvent){}
 }
