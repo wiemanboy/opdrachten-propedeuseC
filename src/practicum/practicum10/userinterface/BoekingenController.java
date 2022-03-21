0073888a9a07c4fc1fafa4e520ab938871c7d83f
J <wiemanboy@gmail.com>
2022-03-21 10:13:22 +0100

    begin 10

---
+++ src/practicum/practicum10/userinterface/BoekingenController.java
@@ -0,0 +1,12 @@
+package practicum.practicum10.userinterface;
+
+import javafx.event.ActionEvent;
+
+public class BoekingenController {
+
+
+
+    public void boek(ActionEvent actionEvent){}
+
+    public void reset(ActionEvent actionEvent){}
+}
